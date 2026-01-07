69fe880a81079a1c52c8e046a5ed46ca385aa852
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:14:32 +0530

    Create ReactivatedReqDataRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/ReactivatedReqDataRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.ReactivatedReqData_DAO;
+
+public interface ReactivatedReqDataRepo extends JpaRepository<ReactivatedReqData_DAO, Long> {
+
+}
