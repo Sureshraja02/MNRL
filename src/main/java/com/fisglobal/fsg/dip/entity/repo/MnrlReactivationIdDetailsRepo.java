9c683f0c5df9bffaddc4f1bcc982bbf16d30ef4a
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:11:56 +0530

    Create MnrlReactivationIdDetailsRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/MnrlReactivationIdDetailsRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.MnrlReactivationIdDetails_DAO;
+
+public interface MnrlReactivationIdDetailsRepo extends JpaRepository<MnrlReactivationIdDetails_DAO, Integer>{
+
+}
