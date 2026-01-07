25f1068faa9b6628a5596ca5e794f65d98683cc8
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 16:23:41 +0530

    Create MnrlAtrEncrypt_Vo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/request/entity/MnrlAtrEncrypt_Vo.java
@@ -0,0 +1,17 @@
+package com.fisglobal.fsg.dip.request.entity;
+
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+
+public class MnrlAtrEncrypt_Vo extends Base_VO {
+
+	private MNRLAtrRequest MNRLATRUpdate_Request;
+
+	public MNRLAtrRequest getMNRLATRUpdate_Request() {
+		return MNRLATRUpdate_Request;
+	}
+
+	public void setMNRLATRUpdate_Request(MNRLAtrRequest mNRLATRUpdate_Request) {
+		MNRLATRUpdate_Request = mNRLATRUpdate_Request;
+	}
+
+}
