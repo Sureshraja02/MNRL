0311f8ea9f3fbc80d436f515a6479f6d4a6bb1f3
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:36:10 +0530

    Create FRICountResponse.java

---
+++ src/main/java/com/fisglobal/fsg/dip/fri/entity/FRICountResponse.java
@@ -0,0 +1,36 @@
+package com.fisglobal.fsg.dip.fri.entity;
+
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class FRICountResponse extends Base_VO{
+
+@SerializedName("count")
+private int count;
+
+@SerializedName("date")
+private String date;
+
+@SerializedName("count")
+public int getCount() {
+return count;
+}
+
+@SerializedName("count")
+public void setCount(int count) {
+this.count = count;
+}
+
+@SerializedName("date")
+public String getDate() {
+return date;
+}
+
+@SerializedName("date")
+public void setDate(String date) {
+this.date = date;
+}
+
+}
