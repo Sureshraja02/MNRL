6cab47a838eb0fe189a786e9e2f79054c5efb6aa
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 16:22:10 +0530

    Create ATRFileRequest_Vo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/request/entity/ATRFileRequest_Vo.java
@@ -0,0 +1,100 @@
+package com.fisglobal.fsg.dip.request.entity;
+
+public class ATRFileRequest_Vo {
+	
+	private String mobile_no;
+	private String lsa_id;
+	private String lsa_description;
+	private String tsp_id;
+	private String tsp_description;
+	private String disconnection_date;
+	private String mnrl_disconnection_reason_decription;
+	private String mnrl_disconnection_reason;	
+	private String action_taken;
+	private String action_remarks;
+	private String action_date;
+	private String disconnection_reason;
+	private String grievance_received;
+	public String getMobile_no() {
+		return mobile_no;
+	}
+	public void setMobile_no(String mobile_no) {
+		this.mobile_no = mobile_no;
+	}
+	public String getLsa_id() {
+		return lsa_id;
+	}
+	public void setLsa_id(String lsa_id) {
+		this.lsa_id = lsa_id;
+	}
+	public String getLsa_description() {
+		return lsa_description;
+	}
+	public void setLsa_description(String lsa_description) {
+		this.lsa_description = lsa_description;
+	}
+	public String getTsp_id() {
+		return tsp_id;
+	}
+	public void setTsp_id(String tsp_id) {
+		this.tsp_id = tsp_id;
+	}
+	public String getTsp_description() {
+		return tsp_description;
+	}
+	public void setTsp_description(String tsp_description) {
+		this.tsp_description = tsp_description;
+	}
+	public String getDisconnection_date() {
+		return disconnection_date;
+	}
+	public void setDisconnection_date(String disconnection_date) {
+		this.disconnection_date = disconnection_date;
+	}
+	public String getMnrl_disconnection_reason_decription() {
+		return mnrl_disconnection_reason_decription;
+	}
+	public void setMnrl_disconnection_reason_decription(String mnrl_disconnection_reason_decription) {
+		this.mnrl_disconnection_reason_decription = mnrl_disconnection_reason_decription;
+	}
+	public String getMnrl_disconnection_reason() {
+		return mnrl_disconnection_reason;
+	}
+	public void setMnrl_disconnection_reason(String mnrl_disconnection_reason) {
+		this.mnrl_disconnection_reason = mnrl_disconnection_reason;
+	}
+	public String getAction_taken() {
+		return action_taken;
+	}
+	public void setAction_taken(String action_taken) {
+		this.action_taken = action_taken;
+	}
+	public String getAction_remarks() {
+		return action_remarks;
+	}
+	public void setAction_remarks(String action_remarks) {
+		this.action_remarks = action_remarks;
+	}
+	public String getAction_date() {
+		return action_date;
+	}
+	public void setAction_date(String action_date) {
+		this.action_date = action_date;
+	}
+	public String getDisconnection_reason() {
+		return disconnection_reason;
+	}
+	public void setDisconnection_reason(String disconnection_reason) {
+		this.disconnection_reason = disconnection_reason;
+	}
+	public String getGrievance_received() {
+		return grievance_received;
+	}
+	public void setGrievance_received(String grievance_received) {
+		this.grievance_received = grievance_received;
+	}
+	
+	
+	
+
+}
