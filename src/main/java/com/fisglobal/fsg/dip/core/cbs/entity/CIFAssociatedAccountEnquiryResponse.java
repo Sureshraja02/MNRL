1f5700d454d18cb3fa51c983072523d143e36c9e
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 11:02:36 +0530

    Create CIFAssociatedAccountEnquiryResponse.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/cbs/entity/CIFAssociatedAccountEnquiryResponse.java
@@ -0,0 +1,38 @@
+
+package com.fisglobal.fsg.dip.core.cbs.entity;
+
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class CIFAssociatedAccountEnquiryResponse extends Base_VO{
+
+	@SerializedName("metaData")
+    private MetaData metaData;
+	@SerializedName("Body")
+    private Body body;
+    
+	@SerializedName("metaData")
+    public MetaData getMetaData() {
+        return metaData;
+    }
+
+	@SerializedName("metaData")
+    public void setMetaData(MetaData metaData) {
+        this.metaData = metaData;
+    }
+
+	@SerializedName("Body")
+    public Body getBody() {
+        return body;
+    }
+
+	@SerializedName("Body")
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    
+
+}
