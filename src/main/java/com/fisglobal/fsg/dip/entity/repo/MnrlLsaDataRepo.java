106b81a09d973a7cbf1cbb3d8bd163658f83c6f6
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:11:06 +0530

    Create MnrlLsaDataRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/MnrlLsaDataRepo.java
@@ -0,0 +1,14 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.fisglobal.fsg.dip.entity.MnrlLsaData_DAO;
+
+public interface MnrlLsaDataRepo extends JpaRepository<MnrlLsaData_DAO, String>{
+	
+	
+
+}
