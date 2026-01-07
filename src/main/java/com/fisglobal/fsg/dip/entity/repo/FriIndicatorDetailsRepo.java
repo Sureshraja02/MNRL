4cbb88aaf9c34c3d5d24bb691b7ba7829c639835
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:07:50 +0530

    Create FriIndicatorDetailsRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/FriIndicatorDetailsRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.FriIndicatorDetails_DAO;
+
+public interface FriIndicatorDetailsRepo extends JpaRepository<FriIndicatorDetails_DAO, String>{
+
+}
