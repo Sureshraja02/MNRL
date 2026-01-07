2af325049559f80bfb4d333faf033d1d36bc6656
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:14:10 +0530

    Create ReactivatedDataRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/ReactivatedDataRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.fisglobal.fsg.dip.entity.ReactivatedData_DAO;
+
+public interface ReactivatedDataRepo extends PagingAndSortingRepository<ReactivatedData_DAO, String>{
+
+}
