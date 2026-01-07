43cd2a5d55b0b2ee8919c32de334a7df7d812069
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 11:04:59 +0530

    Create CIFBasedMobileUpdationResponse.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/cbs/entity/CIFBasedMobileUpdationResponse.java
@@ -0,0 +1,35 @@
+package com.fisglobal.fsg.dip.core.cbs.entity;
+
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+import com.google.gson.annotations.SerializedName;
+
+
+public class CIFBasedMobileUpdationResponse extends Base_VO {
+
+	@SerializedName("metaData")
+	private MetaData metaData;
+	
+	@SerializedName("Body")
+	private Body body;
+
+	@SerializedName("metaData")
+	public MetaData getMetaData() {
+		return metaData;
+	}
+
+	@SerializedName("metaData")
+	public void setMetaData(MetaData metaData) {
+		this.metaData = metaData;
+	}
+
+	@SerializedName("Body")
+	public Body getBody() {
+		return body;
+	}
+
+	@SerializedName("Body")
+	public void setBody(Body body) {
+		this.body = body;
+	}
+
+}
