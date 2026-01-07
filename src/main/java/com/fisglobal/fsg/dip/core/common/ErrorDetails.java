c89fffff75517d12f9bd9df53089f407468f9822
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 11:55:57 +0530

    Create ErrorDetails.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/common/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.fisglobal.fsg.dip.core.common;
+
+import java.util.List;
+
+public class ErrorDetails {
+
+	private List<Error> errorList;
+
+	public List<Error> getErrorList() {
+		return errorList;
+	}
+
+	public void setErrorList(List<Error> errorList) {
+		this.errorList = errorList;
+	}
+
+}
