f76e24976f7e3dbbc0cfdae7d99bfef1f5512667
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 12:11:59 +0530

    Create UserRegistrationRequest.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/data/v1/UserRegistrationRequest.java
@@ -0,0 +1,10 @@
+package com.fisglobal.fsg.dip.core.data.v1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRegistrationRequest {
+
+	private String userID;
+	private String userName; 
+}
