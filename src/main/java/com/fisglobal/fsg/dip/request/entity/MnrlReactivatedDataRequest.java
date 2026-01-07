971b85f71081171a1726ff7100c4f3d8b1e39c4e
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 16:33:21 +0530

    Create MnrlReactivatedDataRequest.java

---
+++ src/main/java/com/fisglobal/fsg/dip/request/entity/MnrlReactivatedDataRequest.java
@@ -0,0 +1,19 @@
+package com.fisglobal.fsg.dip.request.entity;
+
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+
+public class MnrlReactivatedDataRequest extends Base_VO {
+	
+	private MNRLFetchDataRequest MNRLReactiveData_Request;
+
+	public MNRLFetchDataRequest getMNRLReactiveData_Request() {
+		return MNRLReactiveData_Request;
+	}
+
+	public void setMNRLReactiveData_Request(MNRLFetchDataRequest mNRLReactiveData_Request) {
+		MNRLReactiveData_Request = mNRLReactiveData_Request;
+	}
+
+	
+
+}
