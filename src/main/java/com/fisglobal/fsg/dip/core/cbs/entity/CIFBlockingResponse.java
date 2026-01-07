bec3a713f33ff5ef71d70595c4d014f69ada0c8d
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 11:05:40 +0530

    Create CIFBlockingResponse.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/cbs/entity/CIFBlockingResponse.java
@@ -0,0 +1,34 @@
+package com.fisglobal.fsg.dip.core.cbs.entity;
+
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+import com.google.gson.annotations.SerializedName;
+
+public class CIFBlockingResponse extends Base_VO {
+
+	@SerializedName("metaData")
+	private MetaData metaData;
+
+	@SerializedName("Body")
+	private Body body;
+
+	@SerializedName("metaData")
+	public MetaData getMetaData() {
+		return metaData;
+	}
+
+	@SerializedName("metaData")
+	public void setMetaData(MetaData metaData) {
+		this.metaData = metaData;
+	}
+
+	@SerializedName("Body")
+	public Body getBody() {
+		return body;
+	}
+
+	@SerializedName("Body")
+	public void setBody(Body body) {
+		this.body = body;
+	}
+
+}
