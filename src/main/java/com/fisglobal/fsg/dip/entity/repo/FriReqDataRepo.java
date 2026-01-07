99a138185b46992be2f44aef73e6605d4485173d
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:08:12 +0530

    Create FriReqDataRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/FriReqDataRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.FriReqData_DAO;
+
+public interface FriReqDataRepo extends JpaRepository<FriReqData_DAO, Long>{
+
+}
