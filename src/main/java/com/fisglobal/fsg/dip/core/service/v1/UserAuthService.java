ddf79c7846f18c946034fe16ea964ee8b083deb2
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 13:20:08 +0530

    Create UserAuthService.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/service/v1/UserAuthService.java
@@ -0,0 +1,33 @@
+package com.fisglobal.fsg.dip.core.service.v1;
+/*
+ * package com.fisglobal.fsg.mnrl.core.service.v1;
+ * 
+ * import javax.inject.Inject;
+ * 
+ * import org.slf4j.LoggerFactory; import
+ * org.springframework.security.core.userdetails.UserDetails; import
+ * org.springframework.stereotype.Component;
+ * 
+ * import com.fisglobal.fsg.mnrl.core.entity.User_Master; import
+ * com.fisglobal.fsg.mnrl.core.entity.repo.UserRepo;
+ * 
+ * @Component public class UserAuthService {
+ * 
+ * private static final org.slf4j.Logger logger =
+ * LoggerFactory.getLogger(UserAuthService.class);
+ * 
+ * @Inject private UserRepo userRepo;
+ * 
+ * private String username; private String keyword;
+ * 
+ * 
+ * public UserDetails loadUserById(String userId) {
+ * 
+ * User_Master userMaster = userRepo.findByUserID(userId);
+ * 
+ * if(userMaster == null) { logger.error("Username not found: " + userId); }
+ * logger.info("DB ID[{}]",userMaster.getUserID()); return new
+ * CustomUserDetails(userMaster); }
+ * 
+ * }
+ */
