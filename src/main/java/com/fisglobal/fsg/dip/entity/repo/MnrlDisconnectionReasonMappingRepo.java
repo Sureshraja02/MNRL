84785040c46cd305e72d47b43cb91437b3e7d147
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:10:19 +0530

    Create MnrlDisconnectionReasonMappingRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/MnrlDisconnectionReasonMappingRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.MrnlDisconnectionReasonMapping_DAO;
+
+public interface MnrlDisconnectionReasonMappingRepo extends JpaRepository<MrnlDisconnectionReasonMapping_DAO, String>{
+
+}
