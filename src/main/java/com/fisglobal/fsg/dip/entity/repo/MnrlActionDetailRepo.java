dacddae638668a752ce3c363251e1bf36272766b
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:08:41 +0530

    Create MnrlActionDetailRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/MnrlActionDetailRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.MnrlActionDetail_DAO;
+
+public interface MnrlActionDetailRepo extends JpaRepository<MnrlActionDetail_DAO, String>{
+
+}
