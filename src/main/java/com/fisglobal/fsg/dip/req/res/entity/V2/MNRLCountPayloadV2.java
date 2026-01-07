0239e73fa4824dce9255f37ca7140696e84f8828
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:53:33 +0530

    Create MNRLCountPayloadV2.java

---
+++ src/main/java/com/fisglobal/fsg/dip/req/res/entity/V2/MNRLCountPayloadV2.java
@@ -0,0 +1,22 @@
+package com.fisglobal.fsg.dip.req.res.entity.V2;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fisglobal.fsg.dip.core.utils.Base_VO;
+import com.google.gson.annotations.SerializedName;
+
+public class MNRLCountPayloadV2 extends Base_VO {
+	
+	@SerializedName("date")
+	private String date;
+	
+	@SerializedName("date")
+	public String getDate() {
+	return date;
+	}
+
+	@SerializedName("date")
+	public void setDate(String date) {
+	this.date = date;
+	}
+
+}
