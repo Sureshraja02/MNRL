aa49ee8bc82ca8057d41f6b43a58fd6445c8ee6e
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 12:12:21 +0530

    Create UserRequest.java

---
+++ src/main/java/com/fisglobal/fsg/dip/core/data/v1/UserRequest.java
@@ -0,0 +1,78 @@
+package com.fisglobal.fsg.dip.core.data.v1;
+
+public class UserRequest {
+
+	private String userId;
+	private String usrSequrTerm;
+	private String userName;
+	private String userType;
+	private String groupId;
+	private String instid;
+	private String mobileNo;
+	private String email;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUsrSequrTerm() {
+		return usrSequrTerm;
+	}
+
+	public void setUsrSequrTerm(String usrSequrTerm) {
+		this.usrSequrTerm = usrSequrTerm;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getInstid() {
+		return instid;
+	}
+
+	public void setInstid(String instid) {
+		this.instid = instid;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
