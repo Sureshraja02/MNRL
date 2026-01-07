6176e6a455a8b8ba977ce745d620ef808bc6369a
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 16:38:27 +0530

    Create MrnlDecryptedData_VO.java

---
+++ src/main/java/com/fisglobal/fsg/dip/response/entity/MrnlDecryptedData_VO.java
@@ -0,0 +1,18 @@
+package com.fisglobal.fsg.dip.response.entity;
+
+import java.util.List;
+
+public class MrnlDecryptedData_VO {
+	private List<MnrlDecryptDataVo> data;
+
+	public List<MnrlDecryptDataVo> getData() {
+		return data;
+	}
+
+	public void setData(List<MnrlDecryptDataVo> data) {
+		this.data = data;
+	}
+	
+	
+
+}
