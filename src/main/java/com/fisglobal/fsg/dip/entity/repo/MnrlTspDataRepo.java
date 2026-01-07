ebea25018df5ca2823e46f502f195e8cdb0a1a0d
Sureshraja02 <rsureshrajaa@gmail.com>
2026-01-07 15:13:15 +0530

    Create MnrlTspDataRepo.java

---
+++ src/main/java/com/fisglobal/fsg/dip/entity/repo/MnrlTspDataRepo.java
@@ -0,0 +1,9 @@
+package com.fisglobal.fsg.dip.entity.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fisglobal.fsg.dip.entity.MnrlTspData_DAO;
+
+public interface MnrlTspDataRepo extends JpaRepository<MnrlTspData_DAO, Long>{
+
+}
